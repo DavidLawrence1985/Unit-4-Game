a9c3ae85625256a415368187f70986b341112413
David L <dalaw85@gmail.com>
2019-02-19 17:37:39 -0500

    added soudns and timeout so total score is shown before reset

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Unit-4-Game
 crystals collector
+click game made using javaScript and jQuery
+
+object -
+game generates "crystals needed" and match "your crytals" to the total.
+crystals are give a radnom value between 1 and 12.
+click the images of crystals to add to "your crystals". 
+if you match numbers you add a win and if you go over you add a loss.
+
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 function randomNumber(min,max) {
   return  Math.floor(Math.random()*(max-min+1)+min);
 };
+
 // vars
 var wins = 0;
 var losses = 0;
@@ -19,8 +20,9 @@ $("#randomNumber").text(number);
 $("#currentTotal").text(currentTotal);
 $("#wins").text(wins);
 $("#losses").text(losses);
-//onclick images add number to current total 
-// cry click += print to current number
+
+
+//reset random number crystal numbers and and clear current total
 function reset(){
   number = randomNumber(19,120);
   myNum = number;
@@ -31,71 +33,102 @@ function reset(){
   cry2 = randomNumber(1,12);
   cry3 = randomNumber(1,12);
   cry4 = randomNumber(1,12);
-
 }
-
+//sound functions
+function soundOne() {
+  var audio = new Audio('./assets/sounds/chime1.mp3');
+  audio.loop = false;
+  audio.play(); 
+}
+function soundTwo() {
+  var audio = new Audio('./assets/sounds/chime2.mp3');
+  audio.loop = false;
+  audio.play(); 
+}
+function winSound() {
+  var audio = new Audio('./assets/sounds/win.mp3');
+  audio.loop = false;
+  audio.play(); 
+}
+function lossSound() {
+  var audio = new Audio('./assets/sounds/loss.mp3');
+  audio.loop = false;
+  audio.play(); 
+}
+//add crystals and check score
 $("#image1").on("click", function(){
+    soundOne();
     currentTotal =  cry1 + currentTotal;
-    $("#currentTotal").text(currentTotal)
-      if(myNum === currentTotal) {
+    $("#currentTotal").text(currentTotal);
+    if(myNum === currentTotal) {
       wins++;   
       $("#wins").text(wins);
-      reset();
+      winSOund();
+      setTimeout(reset, 250);
     }
-      if(myNum < currentTotal){
+    if(myNum < currentTotal){
       losses++;
       $("#losses").text(losses);
-      reset();
+      lossSound();
+      setTimeout(reset, 250);
     }  
   });
 
-
-   $("#image2").on("click", function(){
-     currentTotal = cry2 + currentTotal; 
-    $("#currentTotal").text(currentTotal) 
-      if(myNum === currentTotal) {
-      wins++;   
-      $("#wins").text(wins); 
-      reset();
-    }
-      if(myNum < currentTotal){
-      losses++;
-      $("#losses").text(losses);
-      reset();
-    }
-   });
-
-   $("#image3").on("click", function(){
-    currentTotal = cry3 + currentTotal;
-    $("#currentTotal").text(currentTotal) 
-      if(myNum === currentTotal) {
+$("#image2").on("click", function(){
+    soundTwo();
+    currentTotal = cry2 + currentTotal; 
+    $("#currentTotal").text(currentTotal);
+    
+    if(myNum === currentTotal) {
       wins++;   
       $("#wins").text(wins);
-      reset();
+      winSound();
+      setTimeout(reset, 250);
     }
-      if(myNum < currentTotal){
+    if(myNum < currentTotal){
       losses++;
       $("#losses").text(losses);
-      reset();
-    }
-   });
-   
-   $("#image4").on("click", function(){
-    currentTotal = cry4 + currentTotal;
-    $("#currentTotal").text(currentTotal)
-      if(myNum === currentTotal) {
-      wins++;   
-      $("#wins").text(wins); 
-      reset();
-    }
-      if(myNum < currentTotal){
-      losses++;
-      $("#losses").text(losses);
-      reset();
+      lossSound();
+      setTimeout(reset, 250);
     }
    });
 
+  $("#image3").on("click", function(){
+    soundOne();
+    currentTotal = cry3 + currentTotal;
+    $("#currentTotal").text(currentTotal); 
+    if(myNum === currentTotal) {
+      wins++;   
+      $("#wins").text(wins);
+      winSound();
+      setTimeout(reset, 250);
+    }
+    if(myNum < currentTotal){
+      losses++;
+      $("#losses").text(losses);
+      lossSound();
+      setTimeout(reset, 250);
+    }
+   });
    
+  $("#image4").on("click", function(){
+    soundTwo();
+    currentTotal = cry4 + currentTotal;
+    $("#currentTotal").text(currentTotal);
+    if(myNum === currentTotal) {
+      wins++;   
+      $("#wins").text(wins);
+      winSound(); 
+      setTimeout(reset, 250);
+    }
+    if(myNum < currentTotal){
+      losses++;
+      $("#losses").text(losses);
+      lossSound();
+      setTimeout(reset, 250);
+    }
+   });
+
 });
 
 
